c6e0c14f7502186e6fa90ea9bd1ff3f4141195ae
Abishan <abishanjeyarajah@gmail.com>
2025-09-01 15:50:48 +0530

    Fix jQuery document ready selector typo
    Replaces '$(document).ready' with '$($document).ready' in the create employee view. This change corrects the selector to ensure the ready event is properly triggered.

--- resources/views/employees/create.blade.php
+++ resources/views/employees/create.blade.php
@@ -29,7 +29,7 @@
 
 {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
 <script>
-    $(document).ready(function() {
+    $($document).ready(function() {
         $("#frm-create").submit(function(e) {
             e.preventDefault();
             $.ajax({
